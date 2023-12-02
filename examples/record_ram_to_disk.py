83d4e730b9927f7c454c004f37da083e88a4b355
Adam Zukerman <adamzukerman@gmail.com>
2023-12-02 11:22:39 -0500

    Respond to inputs

--- examples/record_ram_to_disk.py
+++ examples/record_ram_to_disk.py
@@ -1,6 +1,7 @@
 from pyo import *
 import os
 import time
+from pynput import keyboard as kb
 
 # Audio inputs must be available.
 s = Server(duplex=1).boot()
@@ -13,12 +14,13 @@ path = os.path.join(os.path.expanduser("~"), "Desktop", "synth.wav")
 t = NewTable(length=2, chnls=2, feedback=0.5)
 
 # Retrieves the stereo input
-inp = Input([0, 1])
+inp = Input([0])
 
 # Table recorder. Call rec.play() to start a recording, it stops
 # when the table is full. Call it multiple times to overdub.
-rec = TableRec(inp, table=t, fadetime=0.05).play()
+# rec = TableRec(inp, table=t, fadetime=0.05).play()
 # rec2 = TableRec(inp, table=t).play()
+
 # Reads the content of the table in loop.
 osc = Osc(table=t, freq=t.getRate(), mul=0.5).out()
 
@@ -30,7 +32,31 @@ def recordOver():
     rec2 = TableRec(inp, table=t).play()
 
 # After two seconds, the table content is saved to a file on disk.
-sv = CallAfter(saveToDisk, time=2).play()
-CallAfter(recordOver, time=2)
+# sv = CallAfter(saveToDisk, time=2).play()
+# CallAfter(recordOver, time=2)
 
-s.gui(locals())
+end = False
+#create key listener
+def on_press(key):
+    if key == kb.KeyCode.from_char('r'):
+        recordOver()
+        print("recording again")
+    elif key == kb.KeyCode.from_char('q'):
+        print("ending session")
+        global end 
+        end = True 
+        return False
+    elif key == kb.KeyCode.from_char('s'):
+        print("Starting Pyo Server")
+        s.start()
+    elif key == kb.KeyCode.from_char('S'):
+        print("Stopping Pyo Sever")
+        s.stop()
+    else:
+        print("key behavior not defined")
+
+listener = kb.Listener(on_press=on_press)
+listener.start()
+while listener.is_alive():
+    time.sleep(1)
+# s.gui(locals())
