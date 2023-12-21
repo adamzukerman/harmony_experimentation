c8361910865b3cd119d28871f358be9858af192f
Adam Zukerman <adamzukerman@gmail.com>
2023-12-20 21:34:23 -0500

    more discriptive output

--- system_info.py
+++ system_info.py
@@ -1,7 +1,8 @@
 import pyo
-print(pyo.pa_list_host_apis())
-print(pyo.pa_list_devices())
-print(pyo.pa_get_default_input())
-print(pyo.pa_get_default_output())
-print(pyo.pa_get_output_devices())
+print("host APIs: ", pyo.pa_list_host_apis())
+print("all devices: ", pyo.pa_list_devices())
+print("default input: ", pyo.pa_get_default_input())
+print("default output: ", pyo.pa_get_default_output())
+print("output devices: ", pyo.pa_get_output_devices())
 # print(pyo.pa_get_default_devices_from_host(pyo.host()))
+print("All Done")
